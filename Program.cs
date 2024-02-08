d08c008b83d56afcf59f0aa7fb2f6b5a2a1efa9f
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-02-08 14:33:26 +0400

    Task3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Реализовать вывод массива с помощью рекурсии.
-Console.WriteLine("Enter the Array size");
+/*Console.WriteLine("Enter the Array size");
 string input = Console.ReadLine() ?? "NO DATA";
 
 int IsIntCycle(string input)
@@ -38,5 +38,38 @@ void PrintArray( int[]array, int i = 0)
     Console.Write(array[i] + "\t");
     PrintArray(array, i+1);
 }
-PrintArray(myArray);
+PrintArray(myArray);*/
+//Console.ReadLine();
+// Find the Sum of Array elements with reqursion.
+
+/*int Sum(int[] array , int size , int sum)
+{
+    if (size-1<=0)
+    {
+        sum = sum + array[0];
+        return sum;
+    }
+    sum =  Sum(array, size - 1, sum) + array[size-1];    
+    Console.WriteLine($"After reqursion size = {size} and sum = {sum}");
+    return sum;
+}
+int[] myArray = {1,2,3,4};
+int mySize = myArray.Length;
+int mySum = 0;
+ mySum = Sum(myArray , mySize , mySum );
+Console.WriteLine(mySum);*/
+//Найти сумму цыфр числа с помощью рекурсии.
+int number = 333;
+
+int SumOfDigits(int num, int sum = 0)
+{
+    if (num<=0)
+    {
+        return sum;
+    }
+    sum = sum + num%10 + SumOfDigits(num/10);
+    return sum;
+}
+int mysum = SumOfDigits(number);
+Console.WriteLine(mysum);
 Console.ReadLine();
